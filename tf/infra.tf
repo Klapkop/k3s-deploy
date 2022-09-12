8de07aa89ad5bacd5d6df973088ae29966339523
Jeroen Luchtenberg <jerluchtenberg@gmail.com>
2022-09-12 13:45:53 +0200

    tf: Allow paired ips on server/node ports for loadbalancing

--- tf/infra.tf
+++ tf/infra.tf
@@ -1,6 +1,5 @@
 
 ##### Networking ####
-
 resource "openstack_networking_network_v2" "k3s_network" {
     name = format("%s_k3s_net", var.k3s_cluster_name)
     description = "Managed By Terraform"
@@ -14,14 +13,6 @@ resource "openstack_networking_subnet_v2" "k3s_subnet" {
     ip_version = 4
     cidr = var.k3s_cidr
 }
-
-# resource "openstack_networking_router_v2" "k3s_router" {
-#     name = format("%s_k3s_gw", var.k3s_cluster_name)
-#     description = "Managed By Terraform"
-#     admin_state_up = true
-#     external_network_id = var.os_extnet_id
-  
-# }
 
 resource "openstack_networking_router_interface_v2" "k3s_router_if" {
     router_id = var.os_router_id
@@ -81,7 +72,6 @@ resource "openstack_compute_secgroup_v2" "k3s_secgroup" {
     }
 }
 
-
 resource "openstack_compute_secgroup_v2" "k3s_deploy_secgroup" {
     name = format("%s_deploy", var.k3s_cluster_name)
     description = "Managed By Terraform"
@@ -101,7 +91,6 @@ resource "openstack_compute_secgroup_v2" "k3s_deploy_secgroup" {
         cidr        = "0.0.0.0/0"
     }
 }
-
 
 ## HA setup
 resource "openstack_networking_floatingip_v2" "k3s_floating_vip" {
@@ -126,16 +115,10 @@ resource "openstack_networking_floatingip_associate_v2" "k3s_floating_vip" {
     port_id = "${openstack_networking_port_v2.k3s_vip_port.id}"
 }
 
-#### Instances ####
-resource "openstack_compute_keypair_v2" "k3s_key" {
-    name = format("%s", var.k3s_cluster_name)
-    public_key = var.public_key
-}
-
-# net ports
-resource "openstack_networking_port_v2" "k3s_inst_ports" {
+# Network ports
+resource "openstack_networking_port_v2" "k3s_server_ports" {
     count = var.k3s_server_nodes
-    name = format("k3s_port_%s", count.index)
+    name = format("k3s_serverport_%s", count.index)
     network_id = "${openstack_networking_network_v2.k3s_network.id}"
     security_group_ids = ["${openstack_compute_secgroup_v2.k3s_secgroup.id}"]
 
@@ -145,10 +128,31 @@ resource "openstack_networking_port_v2" "k3s_inst_ports" {
     }
 
     allowed_address_pairs {
-        ip_address = var.k3s_vip
+        ip_address = var.k3s_cidr
     }
 }
 
+resource "openstack_networking_port_v2" "k3s_worker_ports" {
+    count = var.k3s_server_nodes
+    name = format("k3s_workerport_%s", count.index)
+    network_id = "${openstack_networking_network_v2.k3s_network.id}"
+    security_group_ids = ["${openstack_compute_secgroup_v2.k3s_secgroup.id}"]
+
+    fixed_ip {
+        subnet_id = "${openstack_networking_subnet_v2.k3s_subnet.id}"
+        ip_address = format("172.16.1.2%s", count.index)
+    }
+
+    allowed_address_pairs {
+        ip_address = var.k3s_cidr
+    }
+}
+
+### Instances ###
+resource "openstack_compute_keypair_v2" "k3s_key" {
+    name = format("%s", var.k3s_cluster_name)
+    public_key = var.public_key
+}
 
 resource "openstack_compute_instance_v2" "k3s_server_nodes" {
     count = var.k3s_server_nodes
@@ -160,7 +164,7 @@ resource "openstack_compute_instance_v2" "k3s_server_nodes" {
     user_data = file(var.k3s_server_usrdata)
 
     network {
-      port = "${openstack_networking_port_v2.k3s_inst_ports[count.index].id}"
+      port = "${openstack_networking_port_v2.k3s_server_ports[count.index].id}"
     }
 
     metadata = {
@@ -184,8 +188,7 @@ resource "openstack_compute_instance_v2" "k3s_worker_nodes" {
     user_data = file(var.k3s_worker_usrdata)
 
     network {
-      name = "${openstack_networking_network_v2.k3s_network.name}"
-      fixed_ip_v4 = format("172.16.1.2%s", count.index)
+      port = "${openstack_networking_port_v2.k3s_worker_ports[count.index].id}"
     }
 
     metadata = {
@@ -198,19 +201,3 @@ resource "openstack_compute_instance_v2" "k3s_worker_nodes" {
         openstack_networking_router_interface_v2.k3s_router_if
     ]
 }
-
-
-
-
-
-
-
-# resource "openstack_compute_floatingip_associate_v2" "k3s_fips" {
-#      count = var.k3s_server_nodes
-#      floating_ip = "${openstack_networking_floatingip_v2.k3s_ext_ips[count.index].address}"
-#      instance_id = "${openstack_compute_instance_v2.k3s_server_nodes[count.index].id}"
-# }
-    
-    
-
-
